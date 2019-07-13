9c51544996f8ad5079970fa88536722d428fd4d5
12kthn <48074422+12kthn@users.noreply.github.com>
2019-07-13 17:05:33 +0700

    add another project

---
+++ Management_Employee_Local/src/Home/controller/ChamCongController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Home.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author nucle
+ */
+public class ChamCongController implements Initializable{
+
+    @FXML
+    private TableView<?> tblChamCong;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
---
+++ Management_Employee_Local/src/Home/controller/NhanVienController.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Home.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.Observable;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author nucle
+ */
+public class NhanVienController implements Initializable{
+    @FXML
+    private JFXTextField txtGioiTinh;
+
+    @FXML
+    private BarChart<?, ?> chartNumberEmployees;
+
+    @FXML
+    private PieChart chartMaleFemale;
+
+    @FXML
+    private JFXTextField txtTrinhDo;
+
+    @FXML
+    private JFXTextField txtMaNV;
+
+    @FXML
+    private TableView<?> tblEmployees;
+
+    @FXML
+    private JFXTextField txtNgaySinh;
+
+    @FXML
+    private JFXTextField txtHoTen;
+
+    @FXML
+    private JFXTextField txtDiaChi;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtCMND;
+
+    @FXML
+    private JFXTextField txtSoDT;
+    
+    @FXML
+    private JFXComboBox<?> cboPhongBan;
+      
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setPieChart();
+        setBarChart();
+        fillCbo();
+    }
+    
+    private void setPieChart(){
+        
+        PieChart.Data slice1 = new PieChart.Data("Nữ", 16);
+
+        PieChart.Data slice2 = new PieChart.Data("Nam", 20);
+        
+        chartMaleFemale.getData().add(slice1);
+        chartMaleFemale.getData().add(slice2);
+
+//        ObservableList<PieChart.Data> pieChartData = chartMaleFemale.getData();
+//        pieChartData.forEach((data) -> {
+//            data.nameProperty().bind(Bindings.concat(data.pieValueProperty()));
+//        });
+    }
+    
+    private void setBarChart(){
+        XYChart.Series col = new XYChart.Series<>();
+        
+        col.getData().add(new XYChart.Data("Nhân sự", 5));
+        col.getData().add(new XYChart.Data("Kế toáṇ", 4));
+        col.getData().add(new XYChart.Data("Marketting̣", 8));
+        col.getData().add(new XYChart.Data("Kỹ thuậṭ", 3));
+        
+        chartNumberEmployees.getData().add(col);
+    }
+    
+    private void fillCbo(){
+        ObservableList list = FXCollections.observableArrayList("Kế toán","Nhân sự");
+        cboPhongBan.setItems(list);
+    }
+}
