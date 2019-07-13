1f813a6bdcdea71c89029dd0b1acd6008578a01a
12kthn <48074422+12kthn@users.noreply.github.com>
2019-07-13 22:28:46 +0700

    add NhanVien.fxml

--- Management_Employee/src/Home/Main.java
+++ Management_Employee/src/Home/Main.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -17,13 +18,16 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     
+    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Home/gui/Main_FXML.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Home/gui/Main.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
